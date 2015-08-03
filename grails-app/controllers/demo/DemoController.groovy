22c6a2913788075bd3f8be56c2bea9b9361da92d
Jeff Scott Brown <jeff@jeffandbetsy.net>
2015-08-03 02:22:32 -0500

    See https://github.com/grails/grails-core/issues/9159

---
+++ grails-app/controllers/demo/DemoController.groovy
@@ -0,0 +1,8 @@
+package demo
+
+class DemoController {
+
+    def index() {
+        [now: new Date()]
+    }
+}
---
+++ grails-app/views/demo/index.gsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<g:formatDate date="${now}" format="MM/dd/yyyy mm:ss"/>
+</body>
+</html>
